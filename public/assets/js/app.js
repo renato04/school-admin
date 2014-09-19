04acf6e5569ebae84e44996802f3fb5568731721
Renato Ramos Nascimento <ramos.renato@outlook.com>
2014-09-19 15:25:51 -0300

    adicionado loading

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,5 +1,5 @@
 
-var myApp = angular.module('myApp', ['ngRoute']);
+var myApp = angular.module('myApp', ['ngRoute', 'angular-loading-bar']);
 
 myApp.config(['$routeProvider',
   function($routeProvider) {
